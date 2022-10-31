129f321f70e11e7cec54f57b3de0901237a3a3ae
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2022-10-31 19:33:42 -0400

    Fixed api

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-/* movie quotes api fetch
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e41c2950bamsh87b13cf18050365p16b40djsnc1880d0106ba',
-		'X-RapidAPI-Host': 'movies-quotes.p.rapidapi.com'
-	}
-};
-
-fetch('https://movies-quotes.p.rapidapi.com/list', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-// online movie database api fetch 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e41c2950bamsh87b13cf18050365p16b40djsnc1880d0106ba',
-		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-	}
-};
-
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-    */
-
-var userInput = "";
-var userInputparsed = userInput.split("");
-var userInputParsedAPI = userInputparsed.join("%20");
+var userInput = "He is the chosen one";
+var userInputParsed = userInput.split(" ");
+var word = ("");
+var userInputParsedAPI = userInputParsed.join("%20");
+apiString = 'https://movie-quote.p.rapidapi.com/movie/' + userInputParsedAPI;
+var apiCall1 = async function(string1) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3ebc1f76c0mshef9eda572bd8879p1d1b63jsn7d3240cccc5a',
+            'X-RapidAPI-Host': 'movie-quote.p.rapidapi.com'
+        }
+    };
+    let response = await fetch(string1, options)
+    let data = await response.json()
+    //  console.log(data[0])
+    var dataresponse = data[0].title
+    getMovie(dataresponse)
+}
+// var test = apiCall1(apiString);
+// console.log(test);
+var getMovie = function(apiResult) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3ebc1f76c0mshef9eda572bd8879p1d1b63jsn7d3240cccc5a',
+            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+        }
+    };
+     fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${apiResult}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response.results))
+        .catch(err => console.error(err));
+}
+apiCall1(apiString);
