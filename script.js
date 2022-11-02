174eb760f84d86c1b5ee3866523ce4101df61895
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2022-11-02 19:02:42 -0400

    Removed filler code and made minor changes to html and css

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
-console.log("!")
-
 var searchButton = document.getElementById("searchbutton");
 var target = document.querySelector('.tryme');
-var word = ("");
-
+var actorsName = document.querySelector('.actor-name');
+var runTime = document.querySelector('.run-time');
+var year = document.querySelector('.release-date');
 
 var apiCall1 = async function(apiStringArg) {
     let apiString = 'https://movie-quote.p.rapidapi.com/movie/' + apiStringArg;
-    console.log("apiString", apiString)
-
-    //https://juanroldan1989-moviequotes-v1.p.rapidapi.com/api/v1/quotes?actor=Al%20Pacino
-    //let apiString = 'https://movie-quote.p.rapidapi.com/movie/' + userInputParsedAPI;
-    console.log("abc", apiString);
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'ffa3000414msh344555768ec4681p1303dfjsn649283dc7eec',
+            'X-RapidAPI-Key': '5ebb6aa8efmshfac7a21f2e6f2f3p1d6515jsnc1ece5cfcc62',
             'X-RapidAPI-Host': 'movie-quote.p.rapidapi.com'
         }
     }
     let response = await fetch(apiString, options)
     let data = await response.json()
-    //  console.log(data[0])
+    console.log(data)
     var dataresponse = data[0].title
     getMovie(dataresponse)
 }
-// var test = apiCall1(apiString);
-// console.log(test);
+
 var getMovie = async function(apiResult) {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '3ebc1f76c0mshef9eda572bd8879p1d1b63jsn7d3240cccc5a',
+            'X-RapidAPI-Key': 'e41c2950bamsh87b13cf18050365p16b40djsnc1880d0106ba',
             'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
         }
     }
     let response = await fetch(`https://online-movie-database.p.rapidapi.com/title/find?q=${apiResult}`, options)
-    console.log("response", response)
     let data = await response.json();
-    console.log(data.results[0].image["url"]);
+    actors = [];
+    for (let i = 0; i < data.results[0].principals.length; i++) {
+        actorI = data.results[0].principals[i]["legacyNameText"];
+        actors.push(actorI);
+      }
+    console.log("Main Actors: " + actors)
+    console.log("Running Time: " + data.results[0].runningTimeInMinutes)
+    console.log("Year: " + data.results[0].year)
+    var run = data.results[0].runningTimeInMinutes;
+    var date = data.results[0].year;
     var imgresponse = data.results[0].image["url"];
-    console.log(imgresponse);
     var movieImg = document.createElement('img');
     movieImg.setAttribute('src', imgresponse);
     var card = document.createElement('div');
     card.setAttribute('class', 'img-class');
     card.append(movieImg);
     target.append(card);
-        // .then(response => response.json())
-        // .then(response => console.log(response.results))
-        // .catch(err => console.error(err));
+    actorsName.append(actors);
+    runTime.append(run +" Minutes");
+    year.append(date);
+    
 }
 
 searchButton.addEventListener("click", function() {
@@ -59,17 +60,9 @@ searchButton.addEventListener("click", function() {
     var userInputParsed = userInput.split(" ");
     
     var userInputParsedAPI = userInputParsed.join("%20");
-    console.log("userInputParsedAPI", userInputParsedAPI);
     apiCall1(userInputParsedAPI)
 
     }
         
     
 );
-/*
-document.getElementById('')
-console.log(init());
-//searchButton.addEventListener("click", apiCall1(apiString));
-// var movieImg = document.createElement('img');
-// movieImg.append()
-// movieImg.setAttribute('src', 'movieImg bg-primary h-100 text-white'); */
